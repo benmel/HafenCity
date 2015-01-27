e211f9b1f61038adf2dfda9ff42ae1215bc69292
Ben Meline <Ben@Bens-MacBook-Pro.local>
2015-01-26 22:04:45 -0600

    Added map view

--- HafenCity/ViewController.swift
+++ HafenCity/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
