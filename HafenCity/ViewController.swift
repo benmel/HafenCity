03add67a90769a2d5e56eb10b84e077f4fca6c4c
Ben Meline <Ben@Bens-MacBook-Pro.local>
2015-01-26 18:11:55 -0600

    Initial commit from local git

---
+++ HafenCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HafenCity
+//
+//  Created by Ben Meline on 1/26/15.
+//  Copyright (c) 2015 Ben Meline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
