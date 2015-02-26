3a0fb516d3051f549979f1fd83285293932a67b9
Ben Meline <benmeline@gmail.com>
2015-02-25 19:04:00 -0600

    Page view controller with images

--- HafenCity/LocationViewController.swift
+++ HafenCity/LocationViewController.swift
@@ -8,23 +8,108 @@
 
 import UIKit
 
-class LocationViewController: UIViewController {
+class LocationViewController: UIViewController, UIPageViewControllerDataSource {
     
     var annotation: CustomAnnotation?
+    private var pageViewController: UIPageViewController?
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var imagePathLabel: UILabel!
-    @IBOutlet weak var textLabel: UILabel!
-    
+    // Initialize it right away here
+    private let contentImages = [
+        "DSC_0257_s",
+        "DSC_0258_s",
+        "DSC_0259_s",
+        "DSC_0260_s",
+        "DSC_0261_s",
+        "DSC_0262_s",
+        "DSC_0263_s",
+        "DSC_0264_s",
+        "DSC_0265_s",
+        "DSC_0266_s",
+        "DSC_0267_s"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        titleLabel.text = annotation?.title
-        imagePathLabel.text = annotation?.imagePath
-        textLabel.text = annotation?.text
+        createPageViewController()
+        setupPageControl()
     }
     
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as UIPageViewController
+        pageController.dataSource = self
+        pageController.view.frame = self.view.frame
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers: NSArray = [firstController]
+            pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.darkGrayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
+        appearance.backgroundColor = UIColor.blackColor()
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as PageItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as PageItemController
+            pageItemController.itemIndex = itemIndex
+            
+            let imageName = contentImages[itemIndex]
+            let path = NSBundle.mainBundle().pathForResource(imageName, ofType: ".JPG")
+            let image = UIImage(contentsOfFile: path!)
+            pageItemController.image = image
+            
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ HafenCity/PageItemController.swift
@@ -0,0 +1,49 @@
+//
+//  PageItemControllerViewController.swift
+//  HafenCity
+//
+//  Created by Ben Meline on 2/21/15.
+//  Copyright (c) 2015 Ben Meline. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+
+    // MARK: - Variables
+    var itemIndex: Int = 0 // ***
+
+    var image: UIImage? {
+        didSet {
+            self.view.clipsToBounds = true
+            let newImageView = UIImageView(image: image)
+            newImageView.contentMode = .ScaleAspectFill
+            var frame = self.view.frame
+            frame.origin.y = 64
+            newImageView.frame = frame
+            self.view.addSubview(newImageView)
+        }
+    }
+        
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blackColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
