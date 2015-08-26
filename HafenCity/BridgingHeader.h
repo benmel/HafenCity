7cdac62d9696d1359501cf2d3825fa940e9fe065
Ben Meline <benmeline@gmail.com>
2015-08-25 23:49:05 -0700

    Added MWPhotoBrowser

---
+++ HafenCity/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  HafenCity
+//
+//  Created by Ben Meline on 8/25/15.
+//  Copyright (c) 2015 Ben Meline. All rights reserved.
+//
+
+#ifndef HafenCity_BridgingHeader_h
+#define HafenCity_BridgingHeader_h
+
+#import "MWPhotoBrowser.h"
+
+#endif
---
+++ HafenCity/MWHelper.swift
@@ -0,0 +1,9 @@
+//
+//  MWHelper.swift
+//  HafenCity
+//
+//  Created by Ben Meline on 8/25/15.
+//  Copyright (c) 2015 Ben Meline. All rights reserved.
+//
+
+import Foundation
